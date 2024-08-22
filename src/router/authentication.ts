005202648d90e6d57e6895eaece7502498c98288
mrvin100 <vincentyoumssi@gmail.com>
2024-08-22 10:03:19 +0100

    new commit

---
+++ src/router/authentication.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+
+import { register } from '../controllers/authentication'
+import { login } from '../controllers/authentication'
+
+export default (router: express.Router) => {
+    router.post('/auth/register', register)
+    router.post('/auth/login', login)
+}
